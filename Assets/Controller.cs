4fbb98b3e5649e79cc3f35cecf9ffc5ca46980d9
Guillaume <guillaume.tollini@eleves.ec-nantes.fr>
2021-02-04 17:47:44 +0100

    gui premier commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Controller : MonoBehaviour
+{
+    public enum Main
+    {
+        DROITE,
+        GAUCHE
+    }
+
+    public int KaplasLayer = 8;
+
+    public Main main;
+    public double distanceSaisie = 0.1;
+
+    protected List<InputDevice> devices;
+    protected List<InputDevice> hmd;
+
+
+
+    private void Awake()
+    {
+        devices = new List<InputDevice>();
+    }
+
+    void GrabClosestKapla(InputDevice d, int layer, double threshold)
+    {
+        var allGOs = FindObjectsOfType<GameObject>();
+        var kaplas = new List<GameObject>();
+        for (var i = 0; i < allGOs.Length; i++)
+        {
+            if (allGOs[i].layer == layer)
+            {
+                kaplas.Add(allGOs[i]);
+            }
+        }
+        var tab = kaplas.ToArray();
+        double mag = 100.0;
+        int closestIndex = 0;
+        Vector3 v;
+        for (var i = 0; i < tab.Length; i++)
+        {
+            if (d.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 pos))
+            {
+                v = tab[i].transform.position - pos;
+                if (v.magnitude < mag)
+                {
+                    closestIndex = i;
+                    mag = v.magnitude;
+                }
+            }
+        }
+        if (mag < threshold)
+        {
+            GameObject ck = tab[closestIndex];
+            ck.GetComponent<KaplaBehavior>().Grab((d.characteristics & InputDeviceCharacteristics.Right) != 0 ? 1 : 0);
+        }
+        kaplas = null;
+    }
+
+    void ReleaseClosestKapla(InputDevice d, int layer, double threshold)
+    {
+        var allGOs = FindObjectsOfType<GameObject>();
+        var kaplas = new List<GameObject>();
+        for (var i = 0; i < allGOs.Length; i++)
+        {
+            if (allGOs[i].layer == layer)
+            {
+                kaplas.Add(allGOs[i]);
+            }
+        }
+        var tab = kaplas.ToArray();
+        double mag = 100.0;
+        int closestIndex = 0;
+        Vector3 v;
+        for (var i = 0; i < tab.Length; i++)
+        {
+            if (d.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 pos))
+            {
+                v = tab[i].transform.position - pos;
+                if (v.magnitude < mag)
+                {
+                    closestIndex = i;
+                    mag = v.magnitude;
+                }
+            }
+        }
+        if (mag < threshold)
+        {
+            GameObject ck = tab[closestIndex];
+            ck.GetComponent<KaplaBehavior>().Release();
+        }
+        kaplas = null;
+    }
+
+    void Update()
+    {
+        devices.Clear();
+
+
+        InputDeviceCharacteristics x = main == Main.DROITE ? InputDeviceCharacteristics.Right : InputDeviceCharacteristics.Left;
+
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller | x, devices);
+
+        foreach (InputDevice d in devices)
+        {
+            if (d.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 pos))
+            {
+                if (d.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rot))
+                {
+                    transform.localPosition = pos;
+                    transform.localRotation = rot;
+                }
+            }
+
+
+
+            if (d.TryGetFeatureValue(CommonUsages.trigger, out float t) && t > 0.5)
+            {
+                GrabClosestKapla(d, KaplasLayer, distanceSaisie);
+                transform.localScale = 0.1f * Vector3.one;
+            }
+            else
+            {
+                ReleaseClosestKapla(d, KaplasLayer, distanceSaisie);
+                transform.localScale = 0.05f * Vector3.one;
+            }
+        }
+    }
+}
---
+++ Assets/KaplaBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class KaplaBehavior : MonoBehaviour
+{
+    protected bool rightHand;
+
+    protected List<InputDevice> hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isBeingHandled = false;
+        hand = new List<InputDevice>();
+    }
+
+    protected bool isBeingHandled;
+
+    public void Grab(int _rightHand)
+    {
+        isBeingHandled = true;
+        rightHand = _rightHand == 1;
+        this.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void Release()
+    {
+        isBeingHandled = false;
+        this.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHandled)
+        {
+            InputDeviceCharacteristics x = rightHand ? InputDeviceCharacteristics.Right : InputDeviceCharacteristics.Left;
+
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller | x, hand);
+
+            if (hand[0].TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 pos))
+            {
+                if (hand[0].TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rot))
+                {
+                    transform.localPosition = pos;
+                    transform.localRotation = rot;
+                }
+            }
+        }
+    }
+}
